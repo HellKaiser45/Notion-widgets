1ee75674332142672486ac16f0e4a843dbc2d930
HellKaiser45 <root@DESKTOP-VGUFEEC>
2024-11-10 23:52:47 +0100

    added Dockerfile entrypoint and a nginx conf

---
+++ simple-banner/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# Default to localhost if SITE_ORIGIN is not set
+SITE_ORIGIN=${SITE_ORIGIN}
+
+# Build the project using the provided SITE_ORIGIN
+export SITE_ORIGIN
+
+# Prepare nginx configuration
+envsubst '$SITE_ORIGIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Copy built files to nginx directory
+rm -rf /var/www/html/*
+rm -rf /etc/nginx/conf.d/default.conf.tenplate
+cp -r dist/* /var/www/html/
+
+# Start nginx
+exec nginx -g "daemon off;"
